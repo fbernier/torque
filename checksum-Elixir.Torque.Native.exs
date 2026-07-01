df626ecc2ca14e7a0f241a84e6cc28829a7050fc
Louis-Philippe Gauthier <lpgauth@gmail.com>
2026-07-01 18:49:26 -0400

    Add checksums for v0.2.5

--- checksum-Elixir.Torque.Native.exs
+++ checksum-Elixir.Torque.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "libtorque_nif-v0.2.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:d2e89e3766a20f08c0e49da60b741d019bd870192aeb1fc92b60863ca1b4e124",
-  "libtorque_nif-v0.2.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:04e1d95cbb87250bc97d0cf6d9c61c548feb8b3a56c37e03280563a6563b8c94",
-  "libtorque_nif-v0.2.4-nif-2.15-x86_64-apple-darwin--v2.so.tar.gz" => "sha256:c24ff4edd0ae2024e6db1d2b61a08b1d891f8cc2ae7596faba0abe2216424413",
-  "libtorque_nif-v0.2.4-nif-2.15-x86_64-apple-darwin--v3.so.tar.gz" => "sha256:92ba5fd410f414f23aa8e451ddf4712b8ed7d2903328fc1e4d6191339b139c20",
-  "libtorque_nif-v0.2.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:bbdccd9e8af7592f76728a52a057b17e0674f325bc0d8b5c948467569b2bd5b4",
-  "libtorque_nif-v0.2.4-nif-2.15-x86_64-unknown-linux-gnu--v2.so.tar.gz" => "sha256:75a44124a52a706d9f67f9cedd57a51a3dd1864489b0163ff1a7391815fe03a1",
-  "libtorque_nif-v0.2.4-nif-2.15-x86_64-unknown-linux-gnu--v3.so.tar.gz" => "sha256:bc2575462c9d37a21b6749bc369294e4b5fcde5ac2d5b6e9cb1eb08fa2259047",
-  "libtorque_nif-v0.2.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:73e2f7e6dc2a477c54eb97ab7a0964a5a491edbd81174de56fed084c6fb90516",
+  "libtorque_nif-v0.2.5-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:4c3ab92d75c049d48eb388fa3a5e4c6400160d4f96ed9c1978413ac42e6ef52a",
+  "libtorque_nif-v0.2.5-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:64a9c17cd45cc3240dcfeb288b993747f62a04d215872f17835416ccda8aebf6",
+  "libtorque_nif-v0.2.5-nif-2.15-x86_64-apple-darwin--v2.so.tar.gz" => "sha256:771bab2ffee5f09ae07e159dfcf49cf4c7ac0f69dc9d3f50e6022ade3618cbb7",
+  "libtorque_nif-v0.2.5-nif-2.15-x86_64-apple-darwin--v3.so.tar.gz" => "sha256:a0a54e154f5b57795f5c93d386013b9d4496cf361c5a6a041f41d1d8e29b7e96",
+  "libtorque_nif-v0.2.5-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:72df465b535cbfc8b2f9737f9ccac089494784b30d194ab5ff78ed0ece2222e3",
+  "libtorque_nif-v0.2.5-nif-2.15-x86_64-unknown-linux-gnu--v2.so.tar.gz" => "sha256:fab6be5b40445a8d1c9cd98483e07d87d147239bfda9a8c258d43a192c915af2",
+  "libtorque_nif-v0.2.5-nif-2.15-x86_64-unknown-linux-gnu--v3.so.tar.gz" => "sha256:53839e3e74f2edff52a57b18ad0248a62c44e06937b04452288f181972f921bb",
+  "libtorque_nif-v0.2.5-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e8cb9b852be3c901112a19ebdf7266bd063f5e94671d95e92db9e1d7afa0d16a",
 }
